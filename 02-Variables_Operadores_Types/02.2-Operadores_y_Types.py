4291ebea2c7cc27f93893ea34c2bbf1fd9e01473
Boris Savin <h5qvc6vr76@privaterelay.appleid.com>
2026-01-13 23:55:53 -0700

    correcciones en clases-types

--- 02-Variables_Operadores_Types/02.2-Operadores_y_Types.py
+++ 02-Variables_Operadores_Types/02.2-Operadores_y_Types.py
@@ -16,27 +16,39 @@ print(3 // 2)  # Floor division operator(//)
 # type es para indentificar el tipo o clase
 # y estos son los types que existen
 
-print(type(10))                  # Int --------- "Numero Entero"
-print(type(3.14))                # Float ------- "Punto Flotante o Numero Decimal"
-print(type(1 + 3j))              # Complex ----- "Numero Complejo o Ecuacion"
-print(type('Asabeneh'))          # String ------ "Texto (tambien puede abreviarse str)"
-print(type([1, 2, 3]))           # List -------- "lista de datos o variables (conservan el type)"
-print(type({'name':'Asabeneh'})) # Dictionary -- "diccionario (“Si quiero una variable que agrupe datos y a cada uno darle una etiqueta, uso un diccionario.”)"
-print(type({9.8, 3.14, 2.7}))    # Set --------- "conjunto de valores unicos ("no me importa el orden, me importa que no se repitan")"
-print(type((9.8, 3.14, 2.7)))    # Tuple ------- "una agrupacion de datos inmutables (que no deben ser alterados o no deben cambiar)"
-print(type(3 == 3))              # Bool -------- "booleano ("tipo de dato con solo dos valores "True o False" representa condiciones logicas y toma de deciciones(ejemplo: comparaciones)")"
-print(type(3 >= 3))              # Bool -------- "booleano (" == ")"
+my_integer_var = 10
+print(type(my_integer_var))  # <class 'int'> # Int ------------- "Numero Entero"
 
+my_float_var = 4.50
+print(type(my_float_var))  # <class 'float'> # Float ----------- "Punto Flotante o Numero Decimal"
+
+my_string_var = 'hello'
+print(type(my_string_var))  # <class 'str'> # String ----------- "Texto (tambien puede abreviarse str)"
+
+my_boolean_var = True
+print(type(my_boolean_var))  # <class 'bool'> # Bool ----------- "booleano ("tipo de dato con solo dos valores "True o False" representa condiciones logicas y toma de deciciones(ejemplo: comparaciones)")"
+
+my_set_var = {7, 5, 8}
+print(type(my_set_var))  # <class 'set'> # Set ----------------- "conjunto de valores unicos ("no me importa el orden, me importa que no se repitan")"
+
+my_dictionary_var = {'name': 'Alice', 'age': 25}
+print(type(my_dictionary_var))  # <class 'dict'> # Dictionary -- "diccionario (“Si quiero una variable que agrupe datos y a cada uno darle una etiqueta, uso un diccionario.”)"
+
+my_tuple_var = (7, 5, 8)
+print(type(my_tuple_var))  # <class 'tuple'> # Tuple ----------- "una agrupacion de datos inmutables (que no deben ser alterados o no deben cambiar)"
+
+my_range_var = range(5)
+print(type(my_range_var))  # <class 'range'> # Range ----------- "range en Python es una clase que se usa para generar una secuencia de números hasta el numero indicado desde el cero o se especifica inicio y final"
+
+my_list = [22, 'Hello world', 3.14, True]
+print(type(my_list)) # <class 'list'> # List ------------------- "lista de datos o variables (conservan el type)"
+
+my_none_var = None
+print(type(my_none_var))  # <class 'NoneType'>  # None --------- "None se usa para indicar que una variable o función no tiene valor."
 '''
-
-	•	Lista → colección que cambia
-	•	Tupla → colección fija
-	•	Set → valores únicos
-	•	Diccionario → datos etiquetados 
 --------------------------------------------
-Lista = orden
-Tupla = orden fijo
-Set = unicidad
-Diccionario = significado
-
+Lista = orden → colección que cambia
+Tupla = orden fijo → colección fija
+Set = unicidad → valores únicos
+Diccionario = significado → datos etiquetados 
 '''
